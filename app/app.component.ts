2917c657f5f35ba4e51ab1d40ec5ff3f07c7c6c0
Edward Apostol <edward.apostol.github@gmail.com>
2016-09-21 08:22:52 -0400

    #000 : initial commit with toh/ng2 base

---
+++ app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id : number;
+  name : string;
+
+}
+
+@Component({
+  selector: 'my-app',
+  template: `  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <!-- [(ngModel)]="hero.name" is the same as value="{{hero.name}}" -->
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+`
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = {
+    id : 1,
+    name : 'Windstorm'
+  };
+}
